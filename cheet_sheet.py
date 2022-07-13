6b3ce65896568c0a511fff1d452bc0df3fc75325
jluke-russell <jluke.russell@gmail.com>
2022-07-13 15:49:24 -0600

    New code to use

--- cheet_sheet.py
+++ cheet_sheet.py
@@ -34,3 +34,64 @@ yrule = (
 
 # puts it all together 
 bible_chart + xrule + yrule
+
+# Challenge 2 
+
+mister = pd.Series([np.nan, 15, 22, 45, 31, np.nan, 85, 38, 129, 8000, 21, 2])
+mister.median() find median first without NA
+mister.mean() find mean after replacing NA
+
+
+# Challenge 3 
+
+
+
+
+# Challenge 4 
+tidy_flights = (flights
+.replace(-999, np.nan)
+.replace("1500+", 1500)
+.replace("n/a", np.nan).ffill()
+.replace("Febuary", "Feburary")
+)
+tidy_flights.interpolate().reset_index()
+
+
+# Challenge 5 
+import pandas as pd 
+import altair as alt
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import metrics
+
+dwellings_ml = pd.read_csv("https://github.com/byuidatascience/data4dwellings/raw/master/data-raw/dwellings_ml/dwellings_ml.csv")
+
+X_pred = dwellings_ml.drop(dwellings_ml.filter(regex = 'basement|finbsmnt|BASEMENT').columns, axis = 1)
+
+y_pred = dwellings_ml.basement
+y_pred[y_pred > 0] = 1 
+
+
+x = dwellings.filter(['livearea', 'finbsmnt', 
+    'basement', 'nocars', 'numbdrm', 'numbaths', 
+    'stories', 'sprice','gartype_Att','arcstyle_ONE-STORY'])
+y = dwellings['before1980']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 2022)
+
+x_train.head()
+y_train.head()
+
+# RGradient Boosting Classifier
+
+# create the model
+classifier = GradientBoostingClassifier()()
+
+# train the model
+classifier.fit(x_train, y_train)
+
+# make predictions
+y_predictions = classifier.predict(x_test)
